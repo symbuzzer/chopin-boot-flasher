646ab97400162dafcd1a99924c3e584cb9bf1a45
Ali Beyaz <symbuzzer@users.noreply.github.com>
2022-11-24 18:03:27 +0300

    Update chopinbootflasher.bat

--- chopinbootflasher.bat
+++ chopinbootflasher.bat
@@ -1,5 +1,5 @@
 @echo off
-set ver=1.01
+set ver=1.02
 color 0a
 title Easy Boot Flasher v%ver% by Ali BEYAZ
 
@@ -7,10 +7,8 @@ echo.
 set /P "c=Drag and drop kernel *.img file here and press enter button: "
 echo Kernel is on %c%
 pause
-sdk\adb reboot bootloader || @echo "Please authorize adb from your phone and try again" && goto exit
 sdk\fastboot flash boot_ab %c% || @echo "Flash boot error"
 sdk\fastboot set_active a || @echo "set_active a error"
-sdk\adb kill-server
 echo.
 echo Success!
 echo.
