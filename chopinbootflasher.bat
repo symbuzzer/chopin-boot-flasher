8d08088cc00a969a7a246ad80dbba3d54b35a5b0
Ali Beyaz <symbuzzer@users.noreply.github.com>
2022-11-27 11:11:38 +0300

    Update chopinbootflasher.bat

--- chopinbootflasher.bat
+++ chopinbootflasher.bat
@@ -1,5 +1,5 @@
 @echo off
-set ver=1.02
+set ver=1.03
 color 0a
 title Easy Boot Flasher v%ver% by Ali BEYAZ
 
@@ -7,13 +7,13 @@ echo.
 set /P "c=Drag and drop kernel *.img file here and press enter button: "
 echo Kernel is on %c%
 pause
-sdk\fastboot flash boot_ab %c% || @echo "Flash boot error"
-sdk\fastboot set_active a || @echo "set_active a error"
+fastboot flash boot_ab %c% || @echo "Flash boot error"
+fastboot set_active a || @echo "set_active a error"
 echo.
 echo Success!
 echo.
 pause
-sdk\fastboot reboot
+fastboot reboot
 goto exit
 
 :exit
