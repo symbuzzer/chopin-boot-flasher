458df748a380127aad4d3b2abe26899a5867442c
Ali Beyaz <symbuzzer@users.noreply.github.com>
2022-11-27 13:09:23 +0300

    Update chopinbootflasher.bat

--- chopinbootflasher.bat
+++ chopinbootflasher.bat
@@ -1,14 +1,19 @@
 @echo off
-set ver=1.03
+set ver=1.04
 color 0a
-title Easy Boot Flasher v%ver% by Ali BEYAZ
+title Easy Boot Flasher for Chopin v%ver% by Ali BEYAZ
 
 echo.
-set /P "c=Drag and drop kernel *.img file here and press enter button: "
-echo Kernel is on %c%
+set /P "c=Drag and drop KERNEL *.img file here and press enter button: "
+echo Kernel image is on %c%
+echo.
+set /P "d=Drag and drop vbmeta.img file here and press enter button: "
+echo vbmeta image is on %d%
 pause
+
 fastboot flash boot_ab %c% || @echo "Flash boot error"
 fastboot set_active a || @echo "set_active a error"
+fastboot flash vbmeta %d% || @echo "Flash vbmeta error"
 echo.
 echo Success!
 echo.
@@ -19,4 +24,3 @@ goto exit
 :exit
 pause
 exit
-
